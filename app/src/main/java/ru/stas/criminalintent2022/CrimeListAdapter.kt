3c0e1558f84c0705d4acb79823bb3ba894cba7d5
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-05 12:06:32 +0300

    add dirty_cup)

--- app/src/main/java/ru/stas/criminalintent2022/CrimeListAdapter.kt
+++ app/src/main/java/ru/stas/criminalintent2022/CrimeListAdapter.kt
@@ -1,13 +1,23 @@
 package ru.stas.criminalintent2022
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import ru.stas.criminalintent2022.databinding.ListItemCrimeBinding
 
 class CrimeListAdapter(private val crimes : List<Crime>) : RecyclerView.Adapter<CrimeListAdapter.CrimeHolder>(){
 
-    inner class CrimeHolder(val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root)
+
+    inner class CrimeHolder(private val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(crime: Crime){
+            binding.crimeTitle.text = crime.title
+            binding.crimeDate.text = crime.date.toString()
+            binding.crimeDirtyCup.context.getDrawable(R.drawable.dirty_cup)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -15,13 +25,10 @@ class CrimeListAdapter(private val crimes : List<Crime>) : RecyclerView.Adapter<
         return CrimeHolder(binding)
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
-            binding.crimeDirtyCup.resources.getDrawable(R.drawable.dirty_cup)
-        }
+        holder.bind(crime)
     }
 
     override fun getItemCount() = crimes.size
