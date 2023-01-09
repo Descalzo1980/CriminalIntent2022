99f5325aab32b4b68a3c332db20b75c9b5aab6f7
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-09 11:45:11 +0300

    nav, view_models,factory,flow

--- app/src/main/java/ru/stas/criminalintent2022/CrimeListAdapter.kt
+++ app/src/main/java/ru/stas/criminalintent2022/CrimeListAdapter.kt
@@ -1,32 +1,29 @@
 package ru.stas.criminalintent2022
 
-import android.annotation.SuppressLint
-import android.icu.util.Calendar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
 import ru.stas.criminalintent2022.databinding.ListItemCrimeBinding
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 
-class CrimeListAdapter(private val crimes : List<Crime>) : RecyclerView.Adapter<CrimeListAdapter.CrimeHolder>(){
+class CrimeListAdapter(
+    private val crimes : List<Crime>,
+    private val onCrimeClicked: (crimeId: UUID) -> Unit
+    ) : RecyclerView.Adapter<CrimeListAdapter.CrimeHolder>(){
 
 
     inner class CrimeHolder(private val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(crime: Crime) {
+        fun bind(crime: Crime, onCrimeClicked:(crimeId: UUID)->Unit) {
             binding.crimeTitle.text = crime.title
             val current = LocalDateTime.now()
             val simpleDateFormat = DateTimeFormatter.ofPattern(""""EEEE,MMMM dd, yyyy"""")
             val formatted = current.format(simpleDateFormat)
             binding.crimeDate.text = formatted.toString()
             binding.root.setOnClickListener {
-                Snackbar.make(
-                    binding.root,
-                    "${crime.title} clicked!",
-                    Snackbar.LENGTH_SHORT
-                ).show()
+                onCrimeClicked(crime.id)
             }
             binding.crimeSolved.visibility =
                 when {
@@ -42,10 +39,9 @@ class CrimeListAdapter(private val crimes : List<Crime>) : RecyclerView.Adapter<
         return CrimeHolder(binding)
     }
 
-    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.bind(crime)
+        holder.bind(crime,onCrimeClicked)
     }
 
     override fun getItemCount() = crimes.size
