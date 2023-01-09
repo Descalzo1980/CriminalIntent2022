2e6774c2895d8d9969f334ffc39e6aa9e31c305b
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-09 14:03:27 +0300

    done

--- app/src/main/java/ru/stas/CrimeDetailViewModel.kt
+++ app/src/main/java/ru/stas/CrimeDetailViewModel.kt
@@ -29,6 +29,11 @@ class CrimeDetailViewModel(crimeId: UUID):ViewModel() {
             oldCrime?.let{onUpdate(it) }
         }
     }
+
+    override fun onCleared() {
+        super.onCleared()
+            crime.value?.let { crimeRepository.updateCrime(it) }
+    }
 }
 
 class CrimeDetailViewModelFactory(
