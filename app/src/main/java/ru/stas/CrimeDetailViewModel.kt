e50cb59386b2bad986eccf58ecdc44a2b65c5a9e
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-10 10:44:07 +0300

    add handleOnBackPressed(empty title)

--- app/src/main/java/ru/stas/CrimeDetailViewModel.kt
+++ app/src/main/java/ru/stas/CrimeDetailViewModel.kt
@@ -3,7 +3,6 @@ package ru.stas
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import androidx.room.Update
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
--- app/src/main/java/ru/stas/criminalintent2022/CrimeDetailFragment.kt
+++ app/src/main/java/ru/stas/criminalintent2022/CrimeDetailFragment.kt
@@ -1,12 +1,13 @@
 package ru.stas.criminalintent2022
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -16,9 +17,6 @@ import kotlinx.coroutines.launch
 import ru.stas.CrimeDetailViewModel
 import ru.stas.CrimeDetailViewModelFactory
 import ru.stas.criminalintent2022.databinding.FragmentCrimeDetailBinding
-import java.time.format.DateTimeFormatter
-import java.util.*
-
 
 
 class CrimeDetailFragment : Fragment(){
@@ -40,19 +38,32 @@ class CrimeDetailFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCrimeDetailBinding.inflate(inflater,container,false)
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.crimeTitle.text.isEmpty()) {
+                    Toast.makeText(context,"Заполните заголовок",Toast.LENGTH_SHORT).show()
+                } else {
+                    // default onBackPressed() behavior
+                    isEnabled = false // DON'T FORGET THIS!
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.apply {
+
             crimeTitle.doOnTextChanged { text, _, _, _ ->
                 crimeDetailViewModel.updateCrime { oldCrime ->
                     oldCrime.copy(title = text.toString())
                 }
             }
             crimeDate.apply {
-
                 isEnabled = false
             }
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
